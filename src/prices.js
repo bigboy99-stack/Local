0df222b3d0ce8c902b9cf5d54fc853250252ce59
bigboy99-stack <helldogg42@gmail.com>
2025-12-25 17:46:52 -0500

    pricing updated

--- src/prices.js
+++ src/prices.js
@@ -8,15 +8,57 @@ const hb = document.querySelector("#sb-btn")
 const prj = document.querySelector(".prj")
 const pd2 = document.querySelector(".pd2");
 const ema = document.querySelector("#ema");
-// const cs = document.querySelector("#cs");
-// const yt = document.querySelector(".ytd");
-// const vi = document.querySelector(".vi");
+const btc = document.querySelector(".btc");
+const pages = document.querySelector(".pages");
+const num = document.querySelector(".num");
+const total = document.querySelector(".total");
+const host = document.querySelector(".host");
+const select = document.querySelector("select");
+const sa = ["SB","EC","ED","HC","RE","PE","EN","LF"]
+const pm = "PAGES MINIMUM"
+
+select.onchange = ()=>{
+    switch (select.value.toString()) {
+        case sa[0]: pages.textContent = `5 ${pm}`; host.textContent = '100/MONTH';total.textContent = '$2100';
+        break;
+
+        case sa[1]: pages.textContent = `10 ${pm}`; host.textContent = '200/MONTH';total.textContent = '$4200';
+        break;
+        
+        case sa[2]: pages.textContent = `5 ${pm}`; host.textContent = '100/MONTH';total.textContent = '$2100';
+        break;
+
+        case sa[3]: pages.textContent = `5 ${pm}`;host.textContent = '100/MONTH';total.textContent = '$2100';
+        break;
+
+        case sa[4]: pages.textContent = `5 ${pm}`; host.textContent = '100/MONTH';total.textContent = '$2100';
+        break;
+
+        case sa[5]: pages.textContent = `1 PAGE MINIMUM`; host.textContent = '20/MONTH';total.textContent = '$420';
+        break;
+
+        case sa[6]: pages.textContent = `4 ${pm}`; host.textContent = '80/MONTH'; total.textContent = '$1700';
+        break;
+
+        case sa[7]: pages.textContent = `5 ${pm}`; host.textContent = '100/MONTH';total.textContent = '$2100';
+        break;
+    }
+}
+
+
+btc.onclick = () => {
+  btc.select();
+  btc.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(btc.value);
+  alert("bitcoin wallet copied to your clip board " + btc.value);
+}
 
 ema.onclick = () => {
   ema.select();
   ema.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(ema.value);
-  alert("Copied the text: " + ema.value);
+  alert
+  ("Copied the text: " + ema.value);
 }
 function initializeTheme() {
     "on" === localStorage.getItem("dark") ? "true" === ld.getAttribute("aria-checked") || (ld.setAttribute("aria-checked", "true"),
@@ -27,6 +69,7 @@ function initializeTheme() {
     ld.textContent = "dark 🌙",
     ht.className = "bl")
 }
+
 function two() {
     "true" === ld.getAttribute("aria-checked") ? (localStorage.setItem("dark", "off"),
     ld.setAttribute("aria-checked", "false"),
